0f1ef4088bc403754aff528ed8b0d20b8ad8acf0
Diego Ripley <diego@diegoripley.ca>
2026-08-12 17:21:21 +0000

    Add new vintage for National Address Register to download and change download URLs to use d4c-datapkg-foundation Source Cooperative URLs

--- national_address_register/download.sh
+++ national_address_register/download.sh
@@ -2,14 +2,18 @@
 if [ ! -d "${DATA_FOLDER}/national_address_register" ]
 then
     echo "Making directory ${DATA_FOLDER}/national_address_register/"
-    mkdir -p ${DATA_FOLDER}/national_address_register/{input,extracted,output}/{2025-07,2024-12,2024-06,2023,2022}
+    mkdir -p ${DATA_FOLDER}/national_address_register/{input,extracted,output}/{2026-06,2025-12,2025-07,2024-12,2024-06,2023,2022}
 fi
 
 INPUT_FOLDER="${DATA_FOLDER}/national_address_register/input"
 
+echo "Downloading 2026-06 vintage of national address register files"
+aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2026_06.txt" --dir=${INPUT_FOLDER}/2026-06 --auto-file-renaming=false
+echo "Downloading 2025-12 vintage of national address register files"
+aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2025_12.txt" --dir=${INPUT_FOLDER}/2025-12 --auto-file-renaming=false
 echo "Downloading 2025-07 vintage of national address register files"
 aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2025_07.txt" --dir=${INPUT_FOLDER}/2025-07 --auto-file-renaming=false
-echo "Downloading 2024-12 vintage of national address register files"0
+echo "Downloading 2024-12 vintage of national address register files"
 aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2024_12.txt" --dir=${INPUT_FOLDER}/2024-12 --auto-file-renaming=false
 echo "Downloading 2024-06 vintage of national address register files"
 aria2c -x16 -i "${SCRIPT_DIR}/national_address_register/national_address_register_files_2024_06.txt" --dir=${INPUT_FOLDER}/2024-06 --auto-file-renaming=false
